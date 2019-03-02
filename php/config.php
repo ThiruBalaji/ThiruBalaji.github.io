7b423d05eee97578594b17e2c24e8e0f3ec4aa57
Thiru Balaji G <41341817+ThiruBalaji@users.noreply.github.com>
2019-03-03 00:15:18 +0530

    Add files via upload

---
+++ php/config.php
@@ -0,0 +1,19 @@
+<?php
+
+
+//mailto
+$my_email = 'gthirubalaji@gmail.com';
+
+//default sucject email 
+$default_subject = "Your Default Subject Email";
+
+//massage success
+$success_msg = "Thanks, I will get back ";
+
+//captcha
+$active_captcha = TRUE;
+
+//uploader
+$active_uploader = TRUE;
+
+ ?>
